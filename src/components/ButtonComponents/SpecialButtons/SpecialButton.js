6a592a9661f081f1fcd9f27af1a522d0e75a490e
Adrian-Garcia <agarc5@hotmail.com>
2019-08-13 16:03:13 -0500

    added classes to all buttons & divs in components

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 const SpecialButton = (props) => {
   return (
-    <button>
+    <button className="special-buttons">
       {/* Display a button element rendering the data being passed down from the parent container on props */props}
     </button>
   );
